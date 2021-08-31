f93b494a2fcadd7b5490a2850bc67c66b197d547
danae.wardrup <dkwardrup@gmail.com>
2021-08-31 14:23:55 -0600

    try except block for casting integers

--- hodges_status_validation.py
+++ hodges_status_validation.py
@@ -3,17 +3,20 @@ from csv import reader
 
 good_values = [0, 1]
 file_counter = 0
+bad_file_list = []
 
 # user provides path to Recommendation folder
 # path = input('Please enter directory path: ')
 
-path = 'D:\FTP\SmartlinkPROD\Hodges\INBOUND'
+#path = 'D:\FTP\SmartlinkPROD\Hodges\INBOUND'
+path="C:\\Users\\danae.wardrup\\Desktop\\INBOUND"
 all_good_files = True
 for (root, _, files) in os.walk(path):
     # use os.walk read/print directory path and all filenames in said folder
     # skipping subdirectory parameter bc don't need it
     # returns files in a tuple
     for file in files:
+        error_parsing = False
         file_counter += 1  # to identify total files scanned
         bad_file = False
         # for every item in the tuple of filenames...
@@ -27,14 +30,29 @@ for (root, _, files) in os.walk(path):
             # read file contents
             next(csv_reader)  # skip over header since only looking for 0 or 1
             for row in csv_reader:
-                if int(row[1]) not in good_values:  # cast to int bc read as string from csv
-                    bad_file = True
-                    all_good_files = False
+                try:
+                    if int(row[1]) not in good_values:  # cast to int bc read as string from csv
+                        bad_file = True
+                        all_good_files = False
+                except ValueError as e:
+                    if error_parsing == False:
+                        bad_file = True
+                        all_good_files = False
+                        print(f"Could not parse second column in {file_path}")
+                        print("Reason for error: value is not an integer.")
+                        print("-----")
+                        error_parsing = True
         if bad_file:
-            print(file)  # print files with errors
+            bad_file_list.append(file)
+
+print('\n')
 
 if all_good_files:
     print('No errors found')
+else:
+    print('Files with errors found:')
+    for file in bad_file_list:
+        print(file)
 
 print(f'{file_counter} files scanned')  # print total files scanned
 
